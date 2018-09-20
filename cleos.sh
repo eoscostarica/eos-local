50f04d6a6deafe9872d26ff6d881043f669e8aee
Daniel Prado <danazkari@gmail.com>
2018-09-20 12:46:47 -0600

    fix(common): fix docker and cleos executions (#4)

--- cleos.sh
+++ cleos.sh
@@ -4,7 +4,7 @@
 # Go into cmd loop: sudo ./cleos.sh
 # Run single cmd:  sudo ./cleos.sh <cleos paramers>
 
-PREFIX="docker-compose exec nodeosd cleos"
+PREFIX="docker-compose exec keosd cleos --url http://nodeosd:8888"
 if [ -z $1 ] ; then
   while :
   do
