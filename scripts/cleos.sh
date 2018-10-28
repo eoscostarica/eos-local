1ec642a0e57daeee4f0a90bd9bb992737cc11dc5
Gabo Esquivel <contact@gaboesquivel.com>
2018-10-28 17:36:26 -0600

    feat: add gulp as global task manager

--- scripts/cleos.sh
+++ scripts/cleos.sh
@@ -4,7 +4,7 @@
 # Go into cmd loop: sudo ./cleos.sh
 # Run single cmd:  sudo ./cleos.sh <cleos paramers>
 
-PREFIX="docker-compose exec keosd cleos --url http://nodeosd:8888"
+PREFIX="docker-compose exec eosiodev cleos --url http://localhost:8888"
 if [ -z $1 ] ; then
   while :
   do
